924550359d980853cb1130e2d054f6223d6a79fc
oliver mcrobbie <omcrobbie@gmail.com>
2016-03-25 19:15:44 +0000

    finished testing

--- routes.js
+++ routes.js
@@ -1,15 +1,39 @@
 var router = require('express').Router(),
     Employee = require('./db/model');
 
+router.param('id', function(req,res,next,id){
+   Employee.findById(id).then(function(employee){
+      req.employee = employee;
+      next();
+   },next); 
+});
+
 router.route('/')
     .get(function(req,res,next){
         Employee.find().then(function(employees){
             res.json(employees);
-        },next)
+        },next);
     })
     .post(function(req,res,next){
-        Employee.create({name: req.body.name}).then(function(){
-            res.sendStatus(200);
+        Employee.create({name: req.body.name}).then(function(emp){
+            res.json(emp);
+        }, next);
+    });
+
+router.route('/:id')
+    .put(function(req,res,next){
+        var region = req.body.region;
+        if (req.query.remove)
+            req.employee.regions.remove(region);
+        else
+            req.employee.regions.push(region);
+        req.employee.save().then(function(emp){
+            res.json(emp);
+        }, next);
+    })
+    .delete(function(req,res,next){
+        req.employee.remove().then(function(){
+           res.sendStatus(204); 
         }, next);
     });
 
---
+++ test/routes.spec.js
@@ -0,0 +1,47 @@
+var expect = require('chai').expect,
+    request = require('supertest-as-promised')(require('../app')),
+    seed = require('../db/seed');
+
+before(function(){
+    return seed();
+});
+
+describe('Routes tests', function(){
+    var newEmp;
+   it('Gets all employees', function(){
+       return request.get('/api').then(function(res){
+          expect(res.body.length).to.equal(4); 
+       });
+   });
+   it("creates new employee", function(){
+       return request.post('/api').send({name: 'Oliver'}).expect(200)
+       .then(function(res){
+           newEmp = res.body;
+           expect(res.body.name).to.equal('Oliver');
+       });
+   });
+   it('adds region to employee', function(){
+      return request.put('/api/' + newEmp._id).send({region: 'N'})
+      .then(function(res){
+         expect(res.body.regions[0]).to.equal('N'); 
+      }); 
+   });
+   it('removes region from employee', function(){
+      return request.put('/api/'+ newEmp._id + '?remove=true').send({region: 'N'})
+      .then(function(res){
+         expect(res.body.regions.length).to.equal(0); 
+      });
+   });
+   it('removes employee', function(){
+       return request.delete('/api/' + newEmp._id).expect(204)
+       .then(function(){
+          return request.get('/api'); 
+       })
+       .then(function(res){
+          expect(res.body.length).to.equal(4); 
+       });
+   })
+   it('test error handler', function(){
+      return request.get('/api/willfail').expect(404);
+   });
+});
